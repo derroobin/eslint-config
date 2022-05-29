7119d96f76e0514b96ed4bae06573580979381a7
ditsche <marcrobin.richter@posteo.de>
2022-05-29 12:09:48 +0200

    add typescript eslint rules

--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -1,13 +1,16 @@
 module.exports = {
   env: {
-      "browser": true,
-      "commonjs": true,
-      "es6": true,
-      "node": true,
+    "browser": true,
+    "commonjs": true,
+    "es6": true,
+    "node": true,
   },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
   extends: [
       "eslint:recommended",
       "plugin:react/recommended",
+      "plugin:@typescript-eslint/recommended",
       "prettier"
   ],
   rules: {
